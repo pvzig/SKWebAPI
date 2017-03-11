68ce2fd5c6959a48442e2842168ffba3e4c12b66
Peter Zignego <pzignego@gmail.com>
2017-03-10 19:55:24 -0500

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SKWebAPI",
     targets: [
-        Target(name: "SKWebAPI", dependencies: [
-            "SKCore"
-        ])
+        Target(name: "SKWebAPI")
     ],
     dependencies: [
         .Package(url: "https://github.com/SlackKit/SKCore", "4.0.0")
